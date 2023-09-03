d8779cae18200231b604c072fefc147a3b0fdbce
Najim Uddin <engr.najim3@gmail.com>
2023-09-03 11:38:44 +0600

    Prevent duplicate user

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -11,12 +11,26 @@ const RegisterForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!name | !email | !password) {
+    if (!name || !email || !password) {
       setError("All fields are necessary.");
       return;
     }
 
     try {
+      const resUserExists = await fetch("api/userExists", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+      const { user } = await resUserExists.json();
+
+      if (user) {
+        setError("User already exists.");
+        return;
+      }
+
       const res = await fetch("api/register", {
         method: "POST",
         headers: {
